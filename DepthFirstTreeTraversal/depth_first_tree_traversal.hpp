e2d9482a4e4344690a97478d1d243800d9bdfa8a
rumxcola <rumxcola@yandex.ru>
2023-08-14 20:29:02 +0300

    DFS added

--- DepthFirstTreeTraversal/depth_first_tree_traversal.hpp
+++ DepthFirstTreeTraversal/depth_first_tree_traversal.hpp
@@ -1,19 +1,73 @@
 #pragma once
 
-#include <cstddef>
 #include <vector>
+#include <unordered_map>
+#include <queue>
 
 // https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
 
-// Definition for a binary tree node.
+
 struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution {
 public:
-  std::vector<int> distanceK(TreeNode *root, TreeNode *target, int k) {}
+    std::vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        std::unordered_map<TreeNode*, TreeNode*> parent;
+        dfs(root, nullptr, parent);
+
+        std::vector<int> result;
+        std::unordered_map<TreeNode*, bool> visited;
+        std::queue<TreeNode*> q;
+        q.push(target);
+        visited[target] = true;
+
+        int level = 0;
+        while (!q.empty()) {
+            int size = q.size();
+            if (level == k) {
+                for (int i = 0; i < size; ++i) {
+                    result.push_back(q.front()->val);
+                    q.pop();
+                }
+                break;
+            }
+
+            for (int i = 0; i < size; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                if (node->left && !visited[node->left]) {
+                    q.push(node->left);
+                    visited[node->left] = true;
+                }
+                if (node->right && !visited[node->right]) {
+                    q.push(node->right);
+                    visited[node->right] = true;
+                }
+                if (parent[node] && !visited[parent[node]]) {
+                    q.push(parent[node]);
+                    visited[parent[node]] = true;
+                }
+            }
+
+            ++level;
+        }
+
+        return result;
+    }
+
+private:
+    void dfs(TreeNode* node, TreeNode* par, std::unordered_map<TreeNode*, TreeNode*>& parent) {
+        if (!node)
+            return;
+
+        parent[node] = par;
+        dfs(node->left, node, parent);
+        dfs(node->right, node, parent);
+    }
 };
