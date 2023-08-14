4275120971f0ef4feaf631b9e94b6bebfaf7b534
rumxcola <rumxcola@yandex.ru>
2023-08-14 18:39:01 +0300

    selection sort added

--- SelectionSort/selection_sort.hpp
+++ SelectionSort/selection_sort.hpp
@@ -3,7 +3,22 @@
 #include <vector>
 
 // https://leetcode.com/problems/sort-an-array/
-class Solution {
+// class Solution {
 public:
-  std::vector<int> sortArray(std::vector<int> &nums) {}
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n - 1; ++i) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; ++j) {
+                if (nums[j] < nums[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                std::swap(nums[i], nums[minIndex]);
+            }
+        }
+        return nums;
+    }
 };
+
